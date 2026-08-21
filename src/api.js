76d0366a544fa0573c6ed55227cef97316325fef
Carvalho <guilhermeclcosta@hotmail.com>
2026-08-21 10:37:37 -0300

    Alterando código para vercel

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+export const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 /**
  * Helper genérico de requisição. Lança um Error com a mensagem vinda de
